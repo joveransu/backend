95f95d57b5e86de1393b5c9f3b35d5b2a311a770
Anderson Alencar <joveransu@gmail.com>
2022-10-06 10:48:19 -0300

    Criando postagens

--- aula09(interagir-formulario)/app.js
+++ aula09(interagir-formulario)/app.js
@@ -2,27 +2,39 @@ const express = require('express')
 const app = express()
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
+const Post = require('./modulos/post')
 
 //Template Engine
-//Essas duas linhas falam para o express que queremos usar o handlebars como template engine do nosso projeto.
 app.engine('handlebars', handlebars.engine({defaultLayout: 'main'})) //Estamos dizendo que nosso layout é o main views > layouts, precisa ter esses nomes de pastas
 app.set('view engine', 'handlebars')
     
 //Body Parser
-//Configuração para poder pegar dados de um formulario vindo do post
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 //Rotas
+app.get('/', function(req, res){
+    res.render('home')
+})
+
+
 app.get('/cadastrar', function(req, res){
-    //Nome do arquivo abaixo, ele vai detectar que ta na pasta views
     res.render('formulario')
 })
 
-//Precisa alterar o tipo da rota, de get() para post(), pois o formulario ta com method='post'
 app.post('/add', function(req, res){
-    //req.body.title = Vai pegar o title da postagem, OBS: title é o nome do campo definido no formulario.handlebars
-    res.send(`<strong>Titulo:</strong> ${req.body.title}<br><strong>Postagem:</strong> ${req.body.poster}`)
+    try {
+        Post.create({
+            titulo: req.body.title,
+            conteudo: req.body.poster
+        })
+        console.log('Post feito com sucesso!')
+        //Redireciona para outra aba.
+        res.redirect('/')
+    } catch (error) {
+        console.log('Falha ao criar o post.' + error)
+        res.send('Falha ao criar o post.' + error)
+    }
 })
 
 app.listen(8081, function(){
