c63b2540973166cbd83edf69414445b516187785
Anderson Alencar <joveransu@gmail.com>
2022-10-08 22:51:56 -0300

    mongodb

--- aula11(moongose)/app.js
+++ aula11(moongose)/app.js
@@ -1,4 +1,48 @@
 const mongoose = require('mongoose')
 
-//mongoose.connect('mongodb+srv://sample-hostname:27017/?maxPoolSize=20&w=majority')
-mongoose.connect('mongodb://')
+mongoose.Promise = global.Promise;
+// function() é igual a () => {}
+try{
+    mongoose.connect('mongodb://localhost/dados_mongo') //dados_mongo é o nome do banco de dados e ele cria automaticamente
+    console.log('Conexão feita com sucesso!')
+} catch (err) {
+    console.log('ERRO na conexão!' + err)
+}
+
+//Definindo as colunas da tabela.
+const UsuarioSchema = mongoose.Schema({
+    nome: {
+        type: String,
+        require: true
+    },
+    sobrenome: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    idade: {
+        type: String,
+        require: true
+    }
+})
+
+// Criando a tabela usuarios com os campos do UsuarioSchema
+mongoose.model('usuarios', UsuarioSchema)
+
+//Adicionando usuario
+var NewUser = mongoose.model('usuarios')
+
+try{
+    new NewUser({
+        nome: 'Jover',
+        sobrenome: 'Ansu',
+        email: 'joveransu@gmail.com',
+        idade: 23
+    }).save()
+    console.log('Usuario salvo com sucesso.')
+} catch(err){
+    console.log('Houve erro ao registrar um usuario - '+err)
+}
