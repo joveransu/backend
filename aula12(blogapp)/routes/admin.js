197130a0aae5f2b3c345be9d5c17c127782b9b34
Anderson Alencar <joveransu@gmail.com>
2022-10-11 00:10:43 -0300

    criação de postagens

--- aula12(blogapp)/routes/admin.js
+++ aula12(blogapp)/routes/admin.js
@@ -3,14 +3,73 @@ const router = express.Router()
 const mongoose = require('mongoose')
 require('../modulos/Categoria')
 const Categoria = mongoose.model('categorias')
+require('../modulos/Postagem')
+const Postagem = mongoose.model('postagens')
 
 router.get('/', (req, res) => {
     res.render('admin/index')
 })
 
-router.get('/posts', (req, res) => {
-    res.send('Página de posts')
+//Postagens
+
+router.get('/postagens', (req, res) => {
+    res.render('admin/postagens')
 })
+
+router.get('/postagens/add', (req, res) => {
+    Categoria.find().then((categorias) => {
+        res.render('admin/addpostagem', {categorias: categorias})
+    }).catch((err) => {
+        req.flash('error_msg', 'Houve um erro ao carregar as categorias.')
+        res.redirect('/admin')
+    })
+})
+
+router.post('/postagens/nova', (req, res) => {
+    //Validando dados
+    var erros = []
+
+    if(!req.body.titulo && req.body.titulo == undefined || req.body.titulo == null || req.body.titulo.length <= 2){
+        erros.push({texto: "Titulo inválido"}) 
+    }
+
+    if(!req.body.slug && req.body.slug == undefined || req.body.slug == null || req.body.slug.length <= 2){
+        erros.push({texto: "Slug inválido"}) 
+    }
+
+    if(!req.body.descricao && req.body.descricao == undefined || req.body.descricao == null || req.body.descricao.length <= 2){
+        erros.push({texto: "Descrição inválida"}) 
+    }
+
+    if(!req.body.conteudo && req.body.conteudo == undefined || req.body.conteudo == null || req.body.conteudo.length <= 2){
+        erros.push({texto: "Conteúdo inválido"}) 
+    }
+
+    if(req.body.categoria == 0){
+        erros.push({texto: 'Categoria inválida, registre uma categoria.'})
+    }
+
+    if(erros.length > 0){
+        res.render('admin/addpostagem', {erros: erros})
+    }else{
+        const novaPostagem = {
+            titulo: req.body.titulo,
+            slug: req.body.slug,
+            descricao: req.body.descricao,
+            conteudo: req.body.conteudo,
+            categoria: req.body.categoria
+        }
+        new Postagem(novaPostagem).save().then(() => {
+            req.flash('success_msg', 'Postagem criada com sucesso.')
+            res.redirect('/admin/postagens')
+        }).catch((err) => {
+            req.flash('error_msg', 'Houve um erro durante o salvamento da postagem')
+            res.redirect('/admin/postagens')
+        })
+    }
+})
+
+//Categorias
 
 router.get('/categorias', (req, res) => {
     //Ordenar por data -> Categoria.find().sort({date: 'desc'}).then((categorias) => {
